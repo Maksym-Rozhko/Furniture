3d3f1ee4fadb2538f99feea94780ab3b4a41815b
Maksym <maksymRozhko7@gmail.com>
2020-12-23 03:19:43 +0200

    feat: layout completed;
    - added cross-browser compatibility;
    - added smoth scroll top/down;

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,29 @@
 const openBurgerMenu = document.querySelector('.mob-menu');
 const menuSidebar = document.querySelector('.header-inner__lists');
 const burgerLines = document.querySelectorAll('.m-line');
+const mainScreen = document.querySelector('.main-screen');
+const btnScrollDown = document.querySelector('.button-down');
+const btnScrollUp = document.querySelector('.button-up');
+
+btnScrollUp.addEventListener('click', e => {
+    e.preventDefault();
+    btnScrollDown.classList.add('button-scroll');
+    mainScreen.scrollIntoView({
+        behavior: "smooth", 
+        block: "end",
+        inline: "nearest"
+    });
+});
+
+btnScrollDown.addEventListener('click', e => {
+    e.preventDefault();
+    btnScrollDown.classList.remove('button-scroll');
+    btnScrollUp.scrollIntoView({
+        behavior: "smooth", 
+        block: "start",
+        inline: "nearest"
+    });
+});
 
 openBurgerMenu.addEventListener('click', e => {
     e.preventDefault();
@@ -9,3 +32,4 @@ openBurgerMenu.addEventListener('click', e => {
         line.classList.toggle('active');
     }
 });
+
